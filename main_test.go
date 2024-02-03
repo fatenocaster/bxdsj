c0126ae09bb094228a2ca0e239be8caf7a2e7ec5
fatenocaster <1026687734@qq.coom>
2024-02-03 21:12:19 +0800

    fix test ,stop sending msg during 20-8 in beijing time

--- main_test.go
+++ main_test.go
@@ -9,7 +9,22 @@ import (
 
 func TestMain(t *testing.T) {
 	for i := 0; i < 1; i++ {
+		//如果是北京时间的晚上8点到第二天早上8点，就不要请求了
+		//获取当前时间
+		t := time.Now()
+		fmt.Println(t)
+		//获取当前时间的小时
+
+		now := time.Now().In(time.FixedZone("CST", 8*3600)) // Beijing time
+		hour := now.Hour()
+
+		if (hour >= 20) || (hour < 8) {
+			fmt.Println("It's between 8pm and 8am Beijing time.")
+			return
+		}
+
 		resp := timeReserveListApi()
+
 		gotTicket, err := handleResp(resp)
 		if err != nil {
 			fmt.Println("错误信息:", err)
